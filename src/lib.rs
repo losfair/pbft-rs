711d29335cdae65e5874338e2c24c1c8658c87a9
losfair <zhy20000919@hotmail.com>
2018-11-29 12:34:43 +0800

    Improve consensus test

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate serde;
 extern crate serde_derive;
 extern crate bincode;
 extern crate sha2;
+extern crate rand;
 
 pub mod config;
 pub mod host;
--- src/state_test.rs
+++ src/state_test.rs
@@ -2,10 +2,65 @@ use crate::config::Config;
 use crate::host::Host;
 use crate::message::*;
 use crate::state::PbftState;
-use std::collections::HashMap;
+use rand::{thread_rng, RngCore};
+use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread::spawn;
+
+#[derive(Clone, Default)]
+struct MessageSink {
+    messages: Arc<Mutex<HashMap<Vec<u8>, HashSet<String>>>>,
+}
+
+impl MessageSink {
+    fn put_message(&self, node_id: String, message: Vec<u8>) {
+        self.messages
+            .lock()
+            .unwrap()
+            .entry(message)
+            .or_insert(HashSet::new())
+            .insert(node_id);
+    }
+
+    fn check(&self, expected_messages: Option<Vec<Vec<u8>>>, num_nodes: usize) {
+        let messages = self.messages.lock().unwrap();
+
+        if let Some(expected_messages) = expected_messages {
+            for msg in &expected_messages {
+                if let Some(v) = messages.get(msg) {
+                    if v.len() != num_nodes {
+                        panic!(
+                            "v.len() != num_nodes: {} != {}, message = {:?}",
+                            v.len(),
+                            num_nodes,
+                            msg
+                        );
+                    }
+                } else {
+                    panic!("expected message not found: {:?}", msg);
+                }
+            }
+            println!(
+                "verified {}/{} messages",
+                expected_messages.len(),
+                messages.len()
+            );
+        } else {
+            for (k, v) in &*messages {
+                if v.len() != num_nodes {
+                    panic!(
+                        "v.len() != num_nodes: {} != {}, message = {:?}",
+                        v.len(),
+                        num_nodes,
+                        k
+                    );
+                }
+            }
+            println!("verified {} messages", messages.len());
+        }
+    }
+}
 
 struct Payload {
     from: String,
@@ -19,6 +74,7 @@ struct Registry {
 struct NodeHost {
     id: String,
     registry: Arc<Registry>,
+    message_sink: MessageSink,
 }
 
 impl Host for NodeHost {
@@ -40,11 +96,9 @@ impl Host for NodeHost {
     }
 
     fn apply_commit(&mut self, content: &[u8]) {
-        println!(
-            "[{}] CONTENT = {}",
-            self.id,
-            ::std::str::from_utf8(content).unwrap()
-        );
+        println!("[{}] CONTENT = {:?}", self.id, content);
+        self.message_sink
+            .put_message(self.id.clone(), content.to_vec());
     }
 }
 
@@ -58,6 +112,7 @@ fn test_mock_network() {
     ];
     let mut senders: HashMap<String, Mutex<Sender<Payload>>> = HashMap::new();
     let mut receivers: HashMap<String, Option<Receiver<Payload>>> = HashMap::new();
+    let message_sink = MessageSink::default();
 
     for id in &node_ids {
         let (tx, rx) = channel();
@@ -76,6 +131,7 @@ fn test_mock_network() {
             NodeHost {
                 id: id.clone(),
                 registry: registry.clone(),
+                message_sink: message_sink.clone(),
             },
         );
         let receiver = receivers.get_mut(id).unwrap().take().unwrap();
@@ -85,23 +141,37 @@ fn test_mock_network() {
         });
     }
 
-    let sender = registry
-        .nodes
-        .get(&node_ids[1]) // test message forwarding
-        .unwrap()
-        .lock()
-        .unwrap()
-        .clone();
-    sender
-        .send(Payload {
-            from: "".into(),
-            body: ::bincode::serialize(&Message::Init(InitMessage {
-                content: "hello world".to_string().into_bytes(),
-            }))
-            .unwrap(),
+    let mut rng = thread_rng();
+
+    let messages: Vec<Vec<u8>> = (0..100)
+        .map(|_| {
+            let mut v = vec![0; 32];
+            rng.fill_bytes(&mut v);
+            v
         })
-        .unwrap();
+        .collect();
+
+    for msg in &messages {
+        let msg = msg.clone();
+        let sender = registry
+            .nodes
+            .get(&node_ids[(rng.next_u64() % node_ids.len() as u64) as usize])
+            .unwrap()
+            .lock()
+            .unwrap()
+            .clone();
+
+        spawn(move || {
+            sender
+                .send(Payload {
+                    from: "".into(),
+                    body: ::bincode::serialize(&Message::Init(InitMessage { content: msg }))
+                        .unwrap(),
+                })
+                .unwrap();
+        });
+    }
 
     ::std::thread::sleep(::std::time::Duration::from_millis(1000));
-    panic!();
+    message_sink.check(Some(messages), node_ids.len());
 }
